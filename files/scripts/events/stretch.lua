08a10055394cddf5286aea58bf8a16d20d0aa5d3
den3606 <dedendendedenpunn@gmail.com>
2022-11-12 16:27:47 +0900

    fix: stretchをLuaComponent依存でframeCountするように修正した

--- files/scripts/events/stretch.lua
+++ files/scripts/events/stretch.lua
@@ -12,26 +12,26 @@ local function Stretch(data, event)
   -- Note:
   -- Coilでコルーチンの非同期処理を行っており、その中で継続的に処理が走るため、player_entity_idが消失した場合でも走り続けている可能性がある
   -- Entityファイルのlifetimeでwhile処理をかけられるように（Coilを排除できるように）修正した方が良い。
-  Coil.add(function()
-    local scale_size = 0.05
-    local _, _, rotation, scale_x, scale_y = EntityGetTransform(player_entity_id)
-    if scale_x == nil or scale_y == nil or scale_size == nil then
-      return
-    end
-    while (scale_y < 3.0) do
-      local x, y = EntityGetTransform(player_entity_id)
-      scale_y = scale_y + scale_size
-      EntitySetTransform(player_entity_id, x, y, rotation, scale_x, scale_y)
-      Coil.wait(1)
-    end
-    Coil.wait(520)
-    while (scale_y >= 1.0) do
-      local x, y = EntityGetTransform(player_entity_id)
-      scale_y = scale_y - scale_size
-      EntitySetTransform(player_entity_id, x, y, rotation, scale_x, scale_y)
-      Coil.wait(1)
-    end
-  end)
+  -- Coil.add(function()
+  --   local scale_size = 0.05
+  --   local _, _, rotation, scale_x, scale_y = EntityGetTransform(player_entity_id)
+  --   if scale_x == nil or scale_y == nil or scale_size == nil then
+  --     return
+  --   end
+  --   while (scale_y < 3.0) do
+  --     local x, y = EntityGetTransform(player_entity_id)
+  --     scale_y = scale_y + scale_size
+  --     EntitySetTransform(player_entity_id, x, y, rotation, scale_x, scale_y)
+  --     Coil.wait(1)
+  --   end
+  --   Coil.wait(520)
+  --   while (scale_y >= 1.0) do
+  --     local x, y = EntityGetTransform(player_entity_id)
+  --     scale_y = scale_y - scale_size
+  --     EntitySetTransform(player_entity_id, x, y, rotation, scale_x, scale_y)
+  --     Coil.wait(1)
+  --   end
+  -- end)
 
   return true
 end
---
+++ files/scripts/status_effects/stretch_end.lua
@@ -0,0 +1,20 @@
+dofile_once("mods/twitch-point-integration/files/scripts/lib/utilities.lua")
+
+print("stretch_end")
+
+local player_entity_id = GetPlayerEntity()
+if (player_entity_id == nil) then
+  return
+end
+
+local scale_size = 0.05
+
+local _, _, rotation, scale_x, scale_y = EntityGetTransform(player_entity_id)
+
+if (scale_y >= 1.0) then
+  local x, y = EntityGetTransform(player_entity_id)
+  scale_y = scale_y - scale_size
+  EntitySetTransform(player_entity_id, x, y, rotation, scale_x, scale_y)
+else
+  EntityKill(GetUpdatedEntityID())
+end
