e2a0b91dab82f4fbec7806d24a6de16fcfc233c8
den3606 <dedendendedenpunn@gmail.com>
2022-11-14 01:29:20 +0900

    fix: randomAllをnoitaのRandomへ移行した

--- files/scripts/events/random_all.lua
+++ files/scripts/events/random_all.lua
@@ -1,9 +1,33 @@
+local function random(a, b)
+  if not a then a, b = 0, 1 end
+  if not b then b = 0 end
+  return a + math.random() * (b - a)
+end
+
+local function weightedchoice(t)
+  local sum = 0
+  for _, v in pairs(t) do
+    assert(v >= 0, "weight value less than zero")
+    sum = sum + v
+  end
+  assert(sum ~= 0, "all weights are zero")
+  local rnd = Random(sum, 0)
+  for k, v in pairs(t) do
+    if rnd < v then return k end
+    rnd = rnd - v
+  end
+end
+
+
 local function RandomAll(data, events)
   local list = {}
   Lume.each(events, function(event)
     list[event.name] = event.weight
   end)
-  local target_name = Lume.weightedchoice(list)
+
+  local year, month, day, hour, minute, second = GameGetDateAndTimeLocal()
+  SetRandomSeed(second + GameGetFrameNum(), second + GameGetFrameNum())
+  local target_name = weightedchoice(list)
 
   if events[target_name] == nil then
     print("NoitaRandomAllEvent[" .. target_name .. "]: undefined")
