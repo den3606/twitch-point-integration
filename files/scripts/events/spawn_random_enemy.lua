532d1e05b21c25572d36790707b4d4151d5208fd
den3606 <dedendendedenpunn@gmail.com>
2022-11-22 00:39:12 +0900

    fix: パラレルボスを削除した

--- files/scripts/events/spawn_random_enemy.lua
+++ files/scripts/events/spawn_random_enemy.lua
@@ -11,6 +11,14 @@ local function SpawnRandomEnemy(data)
 
 
   local animals = dofile_once("mods/twitch-point-integration/files/scripts/animal_list_from_conjurer.lua")
+
+  -- パラレルボスを召喚対象から外す
+  for index, animal in ipairs(animals) do
+    if animal.name == 'parallel_alchemist' or animal.name == 'parallel_tentacles' then
+      table.remove(animals, index)
+    end
+  end
+
   SetRandomSeed(x + GameGetFrameNum(), y + GameGetFrameNum())
   local target_idnex = Random(1, #animals)
   EntityLoad(animals[target_idnex].path, x, y)
